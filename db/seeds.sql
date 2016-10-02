ffe3d77d16d1472468ad46d4f95a42322de4f4c5
Deo Bissessar <Deo Bissessar>
2016-10-01 23:09:37 -0400

    cannot add node_modules

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Wood Grilled Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
---
+++ server.js
@@ -0,0 +1,10 @@
+/*--
+DEPENDENCIES
+--*/
+var express = require('express');
+
+var app = express();
+
+var methOvrRd = require('method-override');
+var bodyParser = require('body-parser');
+
